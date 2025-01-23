94b302ac9f43fe269c2f894c4b6bde36e7cdcd23
Wael Babay <waelbabay@outlook.com>
2025-01-23 18:24:38 +0100

    perf(): Use pure pipes to prevent unneeded rerenders

--- src/app/shared-ui/price/price.component.ts
+++ src/app/shared-ui/price/price.component.ts
@@ -1,10 +1,11 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { SaleMessagePipe } from "./sale-message.pipe";
 
 @Component({
   selector: 'p[app-price]',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, SaleMessagePipe],
   templateUrl: './price.component.html',
   styleUrl: './price.component.scss',
   host: {
@@ -13,15 +14,6 @@ import { Component, Input } from '@angular/core';
 
 })
 export class PriceComponent {
-  @Input({required: true}) price!: number;
+  @Input({ required: true }) price!: number;
   @Input() salePrice?: number;
-
-  onSaleMessage(): string {
-    console.log('in on Sale Message');
-    if(this.salePrice) {
-      const difference = this.price - this.salePrice;
-      return `You save $${difference.toFixed(2)} each!!`
-    }
-    return '';
-  }
 }
---
+++ src/app/shared-ui/price/sale-message.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SaleMessagePipe } from './sale-message.pipe';
+
+describe('SaleMessagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SaleMessagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
