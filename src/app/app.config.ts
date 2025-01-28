3bf90e9179f4779ca76c2b93da599d9d304ef021
Wael Babay <waelbabay@outlook.com>
2025-01-28 11:09:53 +0100

    feat(): Provide the LoggerService class to the LOGGER_TOKEN and change it to provide the NewLoggerService to show how it is easy to swap out services using the injection token pattern

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppData } from './app.data';
+import { LOGGER_TOKEN } from './tokens/logger-token';
+import { NewLoggerService } from './services/new-logger.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     importProvidersFrom(
       InMemoryWebApiModule.forRoot(AppData, { delay: 3000 })
-    )
+    ),
+    {
+      provide: LOGGER_TOKEN,
+      useClass: NewLoggerService
+    }
   ]
 };
